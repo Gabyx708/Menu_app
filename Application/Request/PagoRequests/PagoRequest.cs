8a46939f6025a3232a64f8e2fbfc703211d15ac7
Gabriel <matiascoria427@gmail.com>
2023-08-25 16:07:44 -0300

    cambio pequeño

---
+++ Application/Request/PagoRequests/PagoRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Request.PagoRequests
+{
+    public class PagoRequest
+    {
+        public Guid idPersonal { get; set; }
+        public List<Guid> Recibos { get; set; }
+    }
+}
