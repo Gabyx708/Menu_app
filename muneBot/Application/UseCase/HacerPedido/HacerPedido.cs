281673878bb3354c290c40bd3260af8af9823826
Gabriel <matiascoria427@gmail.com>
2023-08-30 13:46:01 -0300

    inicio del proyecto de bot para automatizar menues , el mismo es una libreria de clases

---
+++ muneBot/Application/UseCase/HacerPedido/HacerPedido.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces.IPedido;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace muneBot.Application.UseCase.HacerPedido
+{
+    public class HacerPedido
+    {
+        private readonly IPedidoService _services;
+
+        public HacerPedido(IPedidoService services)
+        {
+            _services = services;
+        }
+    }
+}
