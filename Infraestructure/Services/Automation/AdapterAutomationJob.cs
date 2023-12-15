c4ee23b7134ce279dd09c3711aacdffc23168581
Gabriel <matiascoria427@gmail.com>
2023-12-15 15:16:12 -0300

    se debe agregar el disparador de jobs para la automaitzacion

---
+++ Infraestructure/Services/Automation/AdapterAutomationJob.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Services.Automation
+{
+    internal class AdapterAutomationJob
+    {
+        private readonly HttpClient _httpClient;
+        private string _serviceURL;
+
+        public AdapterAutomationJob(HttpClient httpClient, string serviceURL)
+        {
+            _httpClient = httpClient;
+            _serviceURL = serviceURL;
+        }
+
+        public async Task initJob()
+        {
+            return;
+        }
+    }
+}
